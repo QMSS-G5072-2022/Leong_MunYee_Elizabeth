5364cc41338c4e3614a2de73cb8f4d06be62562d
elizabethleong <elizabeth.leong@columbia.edu>
2022-11-02 01:11:44 -0400

    add tests

--- cipher_ml4837/tests/test_cipher_ml4837.py
+++ cipher_ml4837/tests/test_cipher_ml4837.py
@@ -1,1 +1,10 @@
 from cipher_ml4837 import cipher_ml4837
+
+def test_cipher_a(self):
+    assert cipher_ml4837.cipher("Good", 1) == "Hppe", "Should be Hppe"
+
+def test_cipher_b(self):
+    assert cipher_ml4837.cipher("Good", -1) == "Fnnc", "Should be Fnnc"
+
+def test_cipher_c(self):
+    assert cipher_ml4837.cipher("Good!283", 1) == "Hppe!283", "Should be Hppe!283"
